c3da3a1d217b9a8f7f57cc0296a06630facaa7a3
sxdlyfe <99546447+sxdlyfe@users.noreply.github.com>
2026-04-11 23:40:34 +0800

    Refactor Product class properties to camelCase
    Refactor Product class properties to use camelCase and update related methods accordingly.

--- Program.cs
+++ Program.cs
@@ -3,24 +3,24 @@ namespace ShoppingCartSystem
 {
     public class Product
     {
-        public int Id;
-        public string Name;
-        public double Price;
-        public int RemainingStock;
+        public int id;
+        public string name;
+        public double price;
+        public int remainingStock;
 
         public void DisplayProduct()
         {
-            Console.WriteLine($"{Id} {Name} {Price} {RemainingStock}");
+            Console.WriteLine($"{id}   {name,5}                 {price} {remainingStock, -5}");
         }
 
         public double GetItemTotal(int quantity)
         {
-            return Price * quantity;
+            return price * quantity;
         }
 
         public bool HasEnoughStock(int quantity)
         {
-            if (RemainingStock < quantity)
+            if (remainingStock < quantity)
             {
                 Console.WriteLine("No Stock Available.");
                 return false;
@@ -33,15 +33,46 @@ namespace ShoppingCartSystem
 
         public void DeductStock(int quantity)
         {
-            RemainingStock -= quantity;
+            remainingStock -= quantity;
         }
     }
     class Program
-    { 
+    {
         static void Main(string[] args)
         {
+            Console.WriteLine(" ID | NAME | PRICE | REMAINING STOCK ");
+            Product[] products = new Product[]
+            {
+                new Product { id = 1, name = "Toyota Vios 2006", price = 200000, remainingStock = 1},
+                new Product { id = 2, name = "Vios 2006 - 2009 Headlight Pair", price = 6000, remainingStock = 8 },
+                new Product { id = 3, name = "Vios 2006 - 2009 Taillight Pair", price = 8000, remainingStock = 4 },
+            };
+            for (int i = 0; i < products.Length; i++)
+            {
+                products[i].DisplayProduct();
+            }
 
+            while (true)
+            {
+                int number;
+                Console.WriteLine("Enter Product ID: ");
+                while (!int.TryParse(Console.ReadLine(), out number))
+                {
+                    Console.WriteLine("Invalid product ID, please try again: ");
+                }
+
+                int quantity;
+                Console.WriteLine("Enter Quantity: ");
+                while (!int.TryParse(Console.ReadLine(), out quantity))
+                {
+                    Console.WriteLine("Invalid Quantity, please try again: ");
+                }
+
+                for (int i = 0; i < products.Length; ++i)
+                {
+                    if (number && quantity)
+                }
+            }
         }
     }
-
 }
