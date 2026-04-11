2de0895e79b2009e4a6ddd78b1528ad99bf2fcc0
sxdlyfe <99546447+sxdlyfe@users.noreply.github.com>
2026-04-11 20:48:14 +0800

    Add Product class for shopping cart systemA
    Added classes, properties methods. Creating product array next

--- Program.cs
+++ Program.cs
@@ -1,1 +1,47 @@
+using System;
+namespace ShoppingCartSystem
+{
+    public class Product
+    {
+        public int Id;
+        public string Name;
+        public double Price;
+        public int RemainingStock;
 
+        public void DisplayProduct()
+        {
+            Console.WriteLine($"{Id} {Name} {Price} {RemainingStock}");
+        }
+
+        public double GetItemTotal(int quantity)
+        {
+            return Price * quantity;
+        }
+
+        public bool HasEnoughStock(int quantity)
+        {
+            if (RemainingStock < quantity)
+            {
+                Console.WriteLine("No Stock Available.");
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public void DeductStock(int quantity)
+        {
+            RemainingStock -= quantity;
+        }
+    }
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+}
