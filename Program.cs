f37eff8b069450afd0e9cd5fe78fbd98653ea3f7
sxdlyfe <99546447+sxdlyfe@users.noreply.github.com>
2026-04-14 08:20:15 +0800

    Updated, finalization. adding the grand total, checking for logic mistakes

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace ShoppingCartSystem
 
         public void DisplayProduct()
         {
-            Console.WriteLine($"{Id}   {Name,5}                 {Price} {RemainingStock, -5}");
+            Console.WriteLine($"{Id,-5} | {Name,-35} | {Price,10} | {RemainingStock,5}");
         }
 
         public double GetItemTotal(int quantity)
@@ -47,20 +47,35 @@ namespace ShoppingCartSystem
         {
             CartItem[] cart = new CartItem[20];
             int ItemsInCart = 0;
-            
-            Console.WriteLine(" ID | NAME | PRICE | REMAINING STOCK ");
+
+            Console.WriteLine("|======== Car Shop and Auto Parts ======|");
+            Console.WriteLine($"{"ID",-5} | {"NAME",-35} | {"PRICE",10} | {"REMAINING STOCK",15} ");
             Product[] products = new Product[]
             {
                 new Product { Id = 1, Name = "Toyota Vios 2006", Price = 200000, RemainingStock = 1},
                 new Product { Id = 2, Name = "Vios 2006 - 2009 Headlight Pair", Price = 6000, RemainingStock = 8 },
                 new Product { Id = 3, Name = "Vios 2006 - 2009 Taillight Pair", Price = 8000, RemainingStock = 4 },
+                new Product { Id = 4, Name = "Honda Civic 2010", Price = 350000, RemainingStock = 5},
+                new Product { Id = 5, Name = "Toyota Fortuner", Price = 800000, RemainingStock = 3},
+                new Product { Id = 6, Name = "Honda Civic Side Mirror Pair", Price = 3500, RemainingStock = 67},
+                new Product { Id = 7, Name = "Fortuner Bumper Cover", Price = 12000, RemainingStock = 10},
+                new Product { Id = 8, Name = "Lancer Brake Pads Set", Price = 2500, RemainingStock = 0},
+                new Product { Id = 9, Name = "Universal Windshield Wipers", Price = 800, RemainingStock = 100},
+                new Product { Id = 10, Name = "Car Battery 12V ", Price = 4500, RemainingStock = 5},
+                new Product { Id = 11, Name = "Radiator Fan Assembly", Price = 6500, RemainingStock = 13 },
+                new Product { Id = 12, Name = "Spark Plug Set ", Price = 1200, RemainingStock = 1000},
+                new Product { Id = 13, Name = "Lancer Side Mirror Pair", Price = 2800, RemainingStock = 9},
+                new Product { Id = 14, Name = "Oil Filter", Price = 500, RemainingStock = 50},
+                new Product { Id = 15, Name = "Clutch Kit", Price = 8000, RemainingStock = 7},
+
             };
             for (int i = 0; i < products.Length; i++)
             {
                 products[i].DisplayProduct();
             }
 
-            while (true)
+            bool exitShop = false;
+            while (!exitShop)
             {
                 Console.Write("\nEnter Product ID: ");
                 if (int.TryParse(Console.ReadLine(), out int productID))
@@ -69,9 +84,8 @@ namespace ShoppingCartSystem
                     Product SelectedProduct = null;
                     for (int i = 0; i < products.Length; i++)
                     {
-                        if (productID  == products[i].Id)
+                        if (productID == products[i].Id)
                         {
-                            Console.WriteLine("ID is Found.");
                             SelectedProduct = products[i];
                             isFound = true;
                         }
@@ -88,7 +102,7 @@ namespace ShoppingCartSystem
                         }
                         else
                         {
-                            Console.WriteLine("Enter Quantity: ");
+                            Console.Write("Enter Quantity: ");
                             if (int.TryParse(Console.ReadLine(), out int quantity))
                             {
                                 if (quantity > 0)
@@ -135,20 +149,59 @@ namespace ShoppingCartSystem
                 }
                 else
                 {
-                    Console.WriteLine("Product ID is not a number. Try Again: ");
+                    Console.Write("\nProduct ID is not a number. Try Again: ");
                 }
 
-                Console.WriteLine("Continue Shopping? Y/N: ");
+                Console.Write("\nContinue Shopping? Y/N: ");
                 string choice = Console.ReadLine().ToUpper();
+
+                double GrandTotal = 0;
+                while (true)
                 {
                     if (choice == "N")
                     {
+                        for (int i = 0; i < ItemsInCart; i++)
+                        {
+                            double itemTotal = cart[i].product.GetItemTotal(cart[i].quantity);
+                            GrandTotal += itemTotal;
+                            Console.WriteLine($"\n{cart[i].product.Name} - x{cart[i].quantity} - {itemTotal:N2}");
+                        }
+
+                        Console.WriteLine($"Grand Total: {GrandTotal}");
+                        if (GrandTotal >= 5000)
+                        {
+                            double discount = GrandTotal * 0.10;
+                            double finalTotal = GrandTotal - discount;
+                            Console.WriteLine($"Discount: {discount:N2}");
+                            Console.WriteLine($"Final Total: {finalTotal:N2}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nDiscount is not applied.");
+                            Console.WriteLine($"Final Total: {GrandTotal:N2}");
+                        }
+
+                        Console.WriteLine("\n|======= UPDATED STOCK =======|");
+                        Console.WriteLine($"{"ID",-5} | {"NAME",-35} | {"PRICE",10} | {"REMAINING STOCK",15} ");
+                        for (int i = 0; i < products.Length; i++)
+                        {
+                            products[i].DisplayProduct();
+                        }
+                        exitShop = true;
+                        break;
+                    }
+
+                    else if (choice == "Y")
+                    {
+                        Console.WriteLine("\nContinue Shopping....");
                         break;
                     }
 
                     else
                     {
-                        Console.WriteLine("Continuing Shopping....");
+                        Console.WriteLine("Invalid input. Please enter Y or N.");
+                        Console.Write("Continue Shopping? Y/N: ");
+                        choice = Console.ReadLine().ToUpper();
                     }
                 }
             }
